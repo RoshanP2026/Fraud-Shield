f39c5ea20ba9e400d125b302411afe7d01854fd1
Roshan Perera <roshanpererait@gmail.com>
2026-07-06 22:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -543,7 +543,7 @@ elif page == "Analytics":
             values=[class_counts[0], class_counts[1]],
             hole=.4,
             marker_colors=['#34D399', '#F87171']
-        )]).
+
         fig_pie.update_layout(title="Class Distribution")
         st.plotly_chart(fig_pie, use_container_width=True)
 
